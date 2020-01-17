68378d536548997a1402c13de95576de052e32e0
Heather Cronk <heather.cronk@colostate.edu>
2020-01-17 02:59:46 -0800

    Just PBS submission stuff. Handle GC stuff in a prep script that calls qsub

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,7 +1,11 @@
 #PBS -S /bin/bash
 
+#################
+### PBS Stuff ###
+#################
+
 # Select one Broadwell node and use all 28 cores
-#PBS -l select=1:ncpus=28:model=bro
+###PBS -l select=${nnodes}:model=bro
 # Submit job to the devel queue
 #PBS -q devel
 # Send an e-mail on abort
@@ -15,36 +19,5 @@
 # export PBS_O_WORKDIR=~/test-scripts
 export PBS_O_WORKDIR=~/ditl_1/submit_jobs
 cd $PBS_O_WORKDIR
-
-####################################
-### Start GeoCarb Specific Stuff ###
-####################################
-
-### Declare Variables ###
-# Eventually make this a command line option with a default value
-base_data_dir="/nobackup/hcronk/data/process"
-# Eventually loop over the granule dirs and get gran from there
-# NTS: When you add that capability, add a check to make sure all the data is there before starting
-gran="20160324_box1_sa2_chunk001"
-njobs=100
-
-#Get sounding selection file for the given granule
-ls ${base_data_dir}/${gran}/geocarb_L2SEL*.txt &> /dev/null
-ret=$?
-
-if [ $ret -eq 0 ]; then
-    sel_file=($(ls ${base_data_dir}/${gran}/geocarb_L2SEL*.txt))
-else
-    echo "Sounding selection file not found"
-    ls ${base_data_dir}/${gran}/geocarb_L2SEL*.txt
-    exit
-fi
-
-### Set Up Retrieval and Log Directories ###
-ret_dir=${base_data_dir}/${gran}/l2fp_retrievals
-log_dir=${base_data_dir}/${gran}/l2fp_logs_100jobs_1
-
-mkdir -p ${ret_dir}
-mkdir -p ${log_dir}
 
 cat ${sel_file} | parallel -j ${njobs} --sshloginfile $PBS_NODEFILE "cd $PWD;./process_granule_soundings.sh ${base_data_dir} ${gran} ${ret_dir} ${log_dir} {}"
